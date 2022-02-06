6556c583ec589881e2d982aa1dd42673d53a1777
Carlos Cancines <Carlozc90@gmail.com>
2022-02-05 19:32:38 -0500

    AppSalon Inicio

--- public/index.php
+++ public/index.php
@@ -2,9 +2,32 @@
 
 require_once __DIR__ . '/../includes/app.php';
 
+use Controllers\LoginController;
 use MVC\Router;
 
 $router = new Router();
+
+// Iniciar sesion
+$router->get('/',[LoginController::class, 'login']);
+$router->post('/',[LoginController::class, 'login']);
+$router->get('/logout',[LoginController::class, 'logout']);
+
+// recuperdad password
+
+$router->get('/olvide',[LoginController::class, 'olvide']);
+$router->post('/olvide',[LoginController::class, 'olvide']);
+$router->get('/recuperar',[LoginController::class, 'recuperar']);
+$router->post('/recuperar',[LoginController::class, 'recuperar']);
+
+// Crear Cuenta
+$router->get('/crear-cuenta',[LoginController::class, 'crear']);
+$router->post('/crear-cuenta',[LoginController::class, 'crear']);
+
+// Confirmar Cuenta
+$router->get('/confirmar-cuenta',[LoginController::class, 'confirmar']);
+
+$router->get('/mensaje',[LoginController::class, 'mensaje']);
+
 
 
 
